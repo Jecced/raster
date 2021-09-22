808590e4f7cd8caa2211f45de9981309b97d7483
安康 <jecced@gmail.com>
2021-09-22 17:52:04 +0000

    feat: 修改了一下重心坐标的使用

--- raster-go/model3d/scene.go
+++ raster-go/model3d/scene.go
@@ -1,18 +1,23 @@
 package model3d
 
+import "raster-go/gl"
+
 type Scene struct {
-	Screen *Screen
-	Camera *Camera
-	Child  []*Node
+	Screen  *Screen
+	ZBuffer *gl.ZBuffer
+	Camera  *Camera
+	Child   []*Node
 }
 
 func NewScene(width, height int) *Scene {
 	camera := NewCamera()
 	camera.SetPosition(0, 100, 0)
 
+	zbuff := gl.NewZBuffer(width, height, 1)
+
 	screen := NewScreen(width, height)
 
-	return &Scene{Camera: camera, Screen: screen, Child: make([]*Node, 0, 0)}
+	return &Scene{Camera: camera, ZBuffer: zbuff, Screen: screen, Child: make([]*Node, 0, 0)}
 }
 
 func (s *Scene) AddChild(node *Node) {
