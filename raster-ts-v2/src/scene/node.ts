5557fce6c6a88579a52ad3ef68573a9c18408327
王安康 <jecced@gmail.com>
2021-12-29 17:38:07 +0800

    feat: node支持设置vbo

--- raster-ts-v2/src/scene/node.ts
+++ raster-ts-v2/src/scene/node.ts
@@ -3,9 +3,15 @@ import { Base } from "./base";
 import { Mat4 } from "../base/math/mat4";
 import { VertexShader } from "../engine/shader/vertex/vertex-shader";
 import { FragmentShader } from "../engine/shader/fragment/fragment-shader";
+import { VAO } from "../engine/data/vao";
+import { VBO } from "../engine/data/vbo";
+import { VertexUtil } from "../engine/data/vertex-util";
 
 export class Node implements Base {
     private position: Vec4;
+
+    private vbo: VBO;
+    private ebo: number[];
 
     private vs: VertexShader;
     private fs: FragmentShader;
@@ -13,6 +19,19 @@ export class Node implements Base {
     constructor() {
         this.position = new Vec4(0, 0, 0, 1);
         this.setPosition(0, 0, 0);
+    }
+
+    public setVBO(vao: VAO, position: number, uv: number, color: number, normal: number, tangent: number): void {
+        this.vbo = VertexUtil.VAO2VBO(vao, position, uv, color, normal, tangent);
+        this.ebo = vao.indices;
+    }
+
+    public getVBO(): VBO {
+        return this.vbo;
+    }
+
+    public getIndices(): number[] {
+        return this.ebo;
     }
 
     public setPosition(x: number, y: number, z: number) {
