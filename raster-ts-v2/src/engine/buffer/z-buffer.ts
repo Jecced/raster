6362f13c1222806d54a322c2b9015a4aa2e14f2f
王安康 <jecced@gmail.com>
2021-12-31 15:52:42 +0800

    feat: 定义z-buffer

---
+++ raster-ts-v2/src/engine/buffer/z-buffer.ts
@@ -0,0 +1,25 @@
+export interface ZBuffer {
+
+    /**
+     * 测试一个位置的深度信息
+     * @param x
+     * @param y
+     * @param z
+     * @param extra
+     */
+    zTest(x: number, y: number, z: number, extra?: any): boolean;
+
+    /**
+     * 给一个位置设置深度
+     * @param x
+     * @param y
+     * @param z
+     * @param extra
+     */
+    setZ(x: number, y: number, z: number, extra?: any): void;
+
+    /**
+     * 清空所有深度
+     */
+    clear(): void;
+}
