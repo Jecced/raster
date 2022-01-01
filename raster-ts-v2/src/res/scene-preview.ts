a097fa4df9ca6e5091f7a586b3d704ebdf2bd914
王安康 <jecced@gmail.com>
2022-01-01 22:15:14 +0800

    todo: 临时注释掉没有用到的模型解析代码

--- raster-ts-v2/src/res/scene-preview.ts
+++ raster-ts-v2/src/res/scene-preview.ts
@@ -2,42 +2,42 @@ import { Scene } from "../scene/scene";
 import { ModelManage } from "./model-manage";
 
 export class ScenePreview {
-    public static async scene01(): Promise<Scene> {
-        const scene = new Scene();
-        const node = await ModelManage.getNode0();
-        scene.addChild(node);
-        scene.getCamera()
-            .setPosition(0, 10, 20)
-            .lookAt(node.getPosition())
-            .usePerspective()
-            .setNera(-10)
-            .setFar(-20);
-        return scene;
-    }
-
-    public static async scene02(): Promise<Scene> {
-        const scene = new Scene();
-        const node = await ModelManage.getNode1();
-        scene.addChild(node);
-        scene.getCamera()
-            .setPosition(0, 10, 20)
-            .lookAt(node.getPosition())
-            .usePerspective()
-            .setNera(-10)
-            .setFar(-20);
-        return scene;
-    }
-
-    public static async scene03(): Promise<Scene> {
-        const scene = new Scene();
-        const node = await ModelManage.getNode2();
-        scene.addChild(node);
-        scene.getCamera()
-            .setPosition(0, 10, 20)
-            .lookAt(node.getPosition())
-            .usePerspective()
-            .setNera(-10)
-            .setFar(-20);
-        return scene;
-    }
+    // public static async scene01(): Promise<Scene> {
+    //     const scene = new Scene();
+    //     const node = await ModelManage.getNode0();
+    //     scene.addChild(node);
+    //     scene.getCamera()
+    //         .setPosition(0, 10, 20)
+    //         .lookAt(node.getPosition())
+    //         .usePerspective()
+    //         .setNera(-10)
+    //         .setFar(-20);
+    //     return scene;
+    // }
+    //
+    // public static async scene02(): Promise<Scene> {
+    //     const scene = new Scene();
+    //     const node = await ModelManage.getNode1();
+    //     scene.addChild(node);
+    //     scene.getCamera()
+    //         .setPosition(0, 10, 20)
+    //         .lookAt(node.getPosition())
+    //         .usePerspective()
+    //         .setNera(-10)
+    //         .setFar(-20);
+    //     return scene;
+    // }
+    //
+    // public static async scene03(): Promise<Scene> {
+    //     const scene = new Scene();
+    //     const node = await ModelManage.getNode2();
+    //     scene.addChild(node);
+    //     scene.getCamera()
+    //         .setPosition(0, 10, 20)
+    //         .lookAt(node.getPosition())
+    //         .usePerspective()
+    //         .setNera(-10)
+    //         .setFar(-20);
+    //     return scene;
+    // }
 }
