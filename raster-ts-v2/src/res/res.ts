af5bd85c69a96ca61013c6262c966659ae451d19
王安康 <jecced@gmail.com>
2022-01-04 18:29:43 +0800

    feat: 修改loader

--- raster-ts-v2/src/res/res.ts
+++ raster-ts-v2/src/res/res.ts
@@ -1,6 +1,6 @@
 // @ts-nocheck
-// import headObj from "../../res/african/african_head.obj";
-// import headImg from "../../res/african/african_head_diffuse.png";
+import african from "../../res/african/african_head.obj";
+import african_diffuse from "../../res/african/african_head_diffuse.png";
 // import diabloObj from "../../res/diablo3/diablo3_pose.obj";
 // import diabloImg from "../../res/diablo3/diablo3_pose_diffuse.png";
 // import bulbasaurObj from "../../res/bulbasaur/Bulbasaur.obj";
@@ -9,6 +9,10 @@
 import grid from "../../res/comm/grid.png";
 
 export enum ResourcePng {
-    Grid = grid
+    Grid = grid,
+    AfricanDiffuse = african_diffuse,
 }
 
+export enum ResourceObj {
+    African = african
+}
