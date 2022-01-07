241f9c5ae0aaf593060cf8db8294b727ddc54462
王安康 <jecced@gmail.com>
2022-01-07 18:46:25 +0800

    feat: 实现了一个可以循环往复运动的node节点

---
+++ src/scene/custom/loop-move-node.ts
@@ -0,0 +1,52 @@
+import { Node } from "../node";
+import { Scheduler } from "../scheduler/scheduler";
+import { RenderingScheduler } from "../scheduler/rendering-scheduler";
+
+export class LoopMoveNode extends Node implements Scheduler {
+
+    private readonly moveX: number = 0;
+    private readonly moveY: number = 0;
+    private readonly moveZ: number = 0;
+
+    private readonly speed: number = 0;
+
+    /**
+     * 各个轴的移动范围和移动速度
+     * @param x
+     * @param y
+     * @param z
+     * @param speed
+     */
+    constructor(x: number, y: number, z: number, speed: number) {
+        super();
+        RenderingScheduler.reg(this);
+
+        this.moveX = x / 2;
+        this.moveY = y / 2;
+        this.moveZ = z / 2;
+        this.speed = speed;
+    }
+
+    private oX: number;
+    private oY: number;
+    private oZ: number;
+
+    public setPosition(x: number, y: number, z: number) {
+        super.setPosition(x, y, z);
+        this.oX = x;
+        this.oY = y;
+        this.oZ = z;
+    }
+
+    private time = 0;
+
+    update(dt?: number) {
+        this.time += dt * this.speed;
+        const sin = Math.sin(this.time);
+        super.setPosition(
+            this.oX + sin * this.moveX,
+            this.oY + sin * this.moveY,
+            this.oZ + sin * this.moveZ,
+        )
+    }
+}
