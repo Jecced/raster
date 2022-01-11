5dfbf880018eb2f880197e83527ee08dc82d3c55
王安康 <jecced@gmail.com>
2022-01-11 16:06:23 +0800

    feat: 加入非洲人, 烘焙好的法线

--- src/resources/resources.ts
+++ src/resources/resources.ts
@@ -1,6 +1,7 @@
 // @ts-nocheck
 import african from "../../resources/african/african_head.obj";
 import african_diffuse from "../../resources/african/african_head_diffuse.png";
+import african_nm from "../../resources/african/african_head_nm.png";
 import diablo from "../../resources/diablo3/diablo3_pose.obj";
 import diablo_diffuse from "../../resources/diablo3/diablo3_pose_diffuse.png";
 import grid from "../../resources/comm/grid.png";
@@ -12,6 +13,7 @@ import brick_wall_normal from "../../resources/brickwall/brickwall_normal.jpg"
 export enum ResourcePng {
     Grid = grid,
     AfricanDiffuse = african_diffuse,
+    AfricanNm = african_nm,
     DiabloDiffuse = diablo_diffuse,
     EarthMap = earth_map,
     BrickWall = brick_wall,
