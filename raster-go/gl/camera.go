540ffe6cc5a4ca9931111e9a998db63032a14259
安康 <jecced@gmail.com>
2024-10-18 17:06:12 +0000

    feat: 修改LockAt函数

--- raster-go/gl/camera.go
+++ raster-go/gl/camera.go
@@ -13,30 +13,32 @@ type Camera struct {
 func NewCamera() *Camera {
 	camera := &Camera{
 		position: NewVec3f(0, 0, 0),
-		Direct:   NewVec3f(0, 0, 0),
-		Up:       NewVec3f(0, 0, 0),
+		Direct:   NewVec3f(0, 0, -1),
+		Up:       NewVec3f(0, 1, 0),
 	}
 	return camera
 }
 
 func (c *Camera) SetPosition(x, y, z float64) {
-	if c.position == nil {
-		c.position = NewVec3f(x, y, z)
-	} else {
-		c.position.Set(x, y, z)
-	}
+	c.position.Set(x, y, z)
 }
 
 func (c *Camera) Position() (float64, float64, float64) {
 	return c.position.X(), c.position.Y(), c.position.Z()
 }
 
-func (c *Camera) LookAt(node NodeBase) {
+func (c *Camera) LookAt(node NodeBase, up Vec3f) {
 	x1, y1, z1 := node.Position()
 	x2, y2, z2 := c.Position()
 	c.Direct = NewVec3f(x1-x2, y1-y2, z1-z2)
 	c.Direct.Normalize()
-	//c.Up = c.Direct.Clone()
+	// 叉乘, up叉乘-z, 得到一个-y的向量, 取hat
+	parallel := CrossVec3f(&up, c.Direct)
+	parallel.Normalize()
+	// 重新叉乘新的-y方向的parallel, 得到新的up方向, 取hat
+	newUp := CrossVec3f(c.Direct, parallel)
+	newUp.Normalize()
+	c.Up = newUp
 }
 
 // UsePerspective 透视模式
