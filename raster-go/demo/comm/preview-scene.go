b8d600a89e97c443b09c0e73117a9857dc9623a8
安康 <jecced@gmail.com>
2024-11-24 08:06:32 +0000

    feat: 透视的测试

--- raster-go/demo/comm/preview-scene.go
+++ raster-go/demo/comm/preview-scene.go
@@ -40,11 +40,36 @@ func Scene03() *model3d.Scene {
 
 func Scene05() *model3d.Scene {
 
-	scene := model3d.NewScene(500, 500)
+	scene := model3d.NewScene(300, 300)
 
-	scene.AddChild(GetNode2())
+	node1 := GetNode0()
+	node2 := GetNode0()
 
-	scene.AddChild(GetFloor())
+	scene.AddChild(node1)
+	scene.AddChild(node2)
+
+	return scene
+}
+
+func Scene06() *model3d.Scene {
+
+	scene := model3d.NewScene(300, 300)
+
+	node := GetNode2()
+
+	for _, v := range node.Obj.V {
+		v.Add(*gl.NewVec4f(0, -0.5, 0, 0))
+	}
+
+	scene.AddChild(node)
+
+	node2 := GetFloor()
+
+	for _, v := range node2.Obj.V {
+		v.Add(*gl.NewVec4f(0, -0.5, 0, 0))
+	}
+
+	scene.AddChild(node2)
 
 	return scene
 }
