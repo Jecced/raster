cd007556ad10255648a4b885e17d8f474b6b2fa0
安康 <jecced@gmail.com>
2024-12-01 13:32:11 +0000

    feat: 给raster类加入render功能

--- raster-ts/src/engine/raster.ts
+++ raster-ts/src/engine/raster.ts
@@ -1,9 +1,14 @@
+import { ZBuffer } from "./zbuffer/interface";
+import { Color } from "./base/color";
+
 export class Raster {
 
     private readonly ctx: CanvasRenderingContext2D = undefined;
 
-    private readonly height: number;
-    private readonly width: number;
+    private readonly imgData: ImageData;
+
+    readonly height: number;
+    readonly width: number;
 
     constructor() {
         const canvas: HTMLCanvasElement = document.getElementById("canvas") as HTMLCanvasElement;
@@ -14,7 +19,31 @@ export class Raster {
 
         this.width = canvas.width;
 
+        this.imgData = this.ctx.getImageData(0, 0, this.width, this.height);
+
         this.clear();
+    }
+
+    public render(buffer: ZBuffer): void {
+        let index = -1;
+        let color:Color;
+        for (let x = 0; x < this.width; x++) {
+            for (let y = 0; y < this.height; y++) {
+
+                index = y * this.width + x;
+
+                color = buffer.getColor(x, y)
+
+                this.imgData.data[4 * index] = color.r;
+                this.imgData.data[4 * index + 1] = color.g;
+                this.imgData.data[4 * index + 2] = color.b;
+                this.imgData.data[4 * index + 3] = color.a;
+                // this.ctx.fillStyle = color.toHEX();
+                // this.ctx.fillRect(x, y, 1, 1)
+            }
+        }
+
+        this.ctx.putImageData(this.imgData, 0, 0);
     }
 
     public clear(): void {
