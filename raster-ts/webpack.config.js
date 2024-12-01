324295ca7da3d8570dd21f391ebb3d19d6a88ae4
安康 <jecced@gmail.com>
2024-12-01 06:47:11 +0000

    feat: webpack接入css
    # Conflicts:
    #	raster-ts/yarn.lock

--- raster-ts/webpack.config.js
+++ raster-ts/webpack.config.js
@@ -1,40 +1,44 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  entry: './src/index.ts',
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(mtl|png|jpg|obj|tga)$/,
-        use: 'file-loader',
-      },
+    mode: "development",
+    entry: "./src/index.ts",
+    devtool: "inline-source-map",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(mtl|png|jpg|obj|tga)$/,
+                use: "file-loader",
+            },
+            {
+                test:/\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./template/index.html",
+            filename: "index.html",
+        }),
     ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './template/index.html',
-      filename: 'index.html',
-    }),
-  ],
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000,
-  },
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 9000,
+    },
 };
