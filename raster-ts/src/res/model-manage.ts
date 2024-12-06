81dc0a3d2de8e3d84506bbf07d44eac312ba1f9f
安康 <jecced@gmail.com>
2024-12-06 04:58:10 +0000

    feat: 资源管理, 预览场景

--- raster-ts/src/res/model-manage.ts
+++ raster-ts/src/res/model-manage.ts
@@ -1,14 +1,42 @@
-import { ObjModel } from "../model/obj/load-obj-model";
 import { Res } from "./res";
+import { Node } from "../scene/node";
 
 export class ModelManage {
 
-    public static async getHeadModel(): Promise<ObjModel> {
+    // 非洲人
+    public static async getNode0(): Promise<Node> {
 
         const model = await Res.loadHeadObj();
 
         model.setMatUV("default", await Res.loadHeadImg());
 
-        return model;
+        return new Node(model);
+    }
+
+    // 大菠萝
+    public static async getNode1(): Promise<Node> {
+
+        const model = await Res.loadDiabloObj();
+
+        model.setMatUV("default", await Res.loadDiabloImg());
+
+        return new Node(model);
+    }
+
+
+    // 妙蛙种子
+    public static async getNode2(): Promise<Node> {
+
+        const model = await Res.loadBulbasaurObj();
+
+        model.setMatUV("mat1", await Res.loadBulbasaurImg1());
+        model.setMatUV("mat2", await Res.loadBulbasaurImg2());
+
+        // 缩放一下
+        for (let i = 0; i < model.v.length; i++) {
+            model.v[i].scale(0.2)
+        }
+
+        return new Node(model);
     }
 }
