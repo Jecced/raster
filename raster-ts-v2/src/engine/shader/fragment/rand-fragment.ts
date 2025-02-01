3c1c0bfe65d436c9dec9b097a347d4d72f5bc77f
王安康 <jecced@gmail.com>
2025-02-01 08:37:51 +0000

    feat: 定义着色器之间传输的变量类, 后面需要做自动插值计算

--- raster-ts-v2/src/engine/shader/fragment/rand-fragment.ts
+++ raster-ts-v2/src/engine/shader/fragment/rand-fragment.ts
@@ -1,13 +1,13 @@
 import { FragmentShader } from "./fragment-shader";
 import { GlData } from "../../data/gl-data";
-import { VAO } from "../../data/vao";
 import { Vec4 } from "../../../base/math/vec4";
+import { ShaderVariable } from "../../data/shader-variable";
 
 /**
  * 不做任何运算, 随机返回一个颜色的着色器
  */
 export class RandFragment implements FragmentShader {
-    main(glData: GlData, input: VAO): Vec4 {
+    main(glData: GlData, input: ShaderVariable): Vec4 {
         return new Vec4(
             Math.random(),
             Math.random(),
