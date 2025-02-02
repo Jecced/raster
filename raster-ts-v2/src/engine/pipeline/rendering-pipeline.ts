3fc075d26fea06f8a2e4a60371c049d5624465ac
王安康 <jecced@gmail.com>
2025-02-02 11:08:00 +0000

    feat: 修改渲染管线

--- raster-ts-v2/src/engine/pipeline/rendering-pipeline.ts
+++ raster-ts-v2/src/engine/pipeline/rendering-pipeline.ts
@@ -1,5 +1,12 @@
 import { VertexShader } from "../shader/vertex/vertex-shader";
 import { FragmentShader } from "../shader/fragment/fragment-shader";
+import { VBO } from "../data/vbo";
+import { FrameBuffer } from "../buffer/frame-buffer";
+import { ZBuffer } from "../buffer/z-buffer";
+import { GlData } from "../data/gl-data";
+import { ShaderVariable } from "../data/shader-variable";
+import { Vec4 } from "../../base/math/vec4";
+import { Calc } from "../../base/math/calc";
 
 /**
  * 渲染管线
@@ -9,7 +16,7 @@ export class RenderingPipeline {
      * 顶点着色器
      * @private
      */
-    private vs: VertexShader;
+    public vs: VertexShader;
     /**
      * 省略内容
      * 曲面细分
@@ -17,25 +24,73 @@ export class RenderingPipeline {
      */
 
     /**
-     * 图元组装
-     * @private
+     * TODO
+     * 图元组装 PrimitiveAssembly
      */
-    // private pa: PrimitiveAssembly;
 
     /**
+     * TODO
      * Culling and Clipping
      */
 
     /**
+     * TODO
      * Rasterization
      */
 
     /**
      * Fragment Shader
      */
-    private fs: FragmentShader;
+    public fs: FragmentShader;
 
     /**
+     * TODO
      * AlphaTest And Blend
      */
+
+    /**
+     * FrameBuffer
+     */
+    public frameBuffer: FrameBuffer;
+
+    /**
+     * ZBuffer
+     */
+    public zBuffer: ZBuffer;
+
+    /**
+     * 渲染一个模型信息, 返回frame buffer
+     * @param vbo
+     * @param ebo
+     * @param glData
+     */
+    public run(vbo: VBO, ebo: number[], glData: GlData): Float32Array {
+        // 顶点, 都先执行一次顶点着色器
+        const vertexCount = vbo.getVertexCount();
+        // 着色器传递变量
+        const variable: ShaderVariable[] = new Array(vertexCount);
+        // 顶点着色器执行后的位置
+        const positions: Vec4[] = new Array(vertexCount);
+        for (let i = 0; i < vertexCount; i++) {
+            variable[i] = new ShaderVariable();
+            positions[i] = this.vs.main(glData, vbo.getVertex(i), variable[i]);
+        }
+        
+        // 遍历三角形的面
+        for (let i = 0, len = ebo.length / 3; i < len; i++) {
+            let i0 = ebo[i * 3];
+            let i1 = ebo[i * 3 + 1];
+            let i2 = ebo[i * 3 + 2];
+            const p0 = positions[i0];
+            const p1 = positions[i1];
+            const p2 = positions[i2];
+            const o0 = Calc.mat4MulVec4(glData.matOrthographic, p0);
+            const o1 = Calc.mat4MulVec4(glData.matOrthographic, p1);
+            const o2 = Calc.mat4MulVec4(glData.matOrthographic, p2);
+            console.log(p0.toString(), o0.toString());
+            console.log(p1.toString(), o1.toString());
+            console.log(p2.toString(), o2.toString());
+        }
+        return undefined;
+    }
 }
