2fea77b1b4867eb97adf2d27ef94de3030bdcad2
王安康 <jecced@gmail.com>
2025-02-02 11:07:27 +0000

    feat: 变量修改为根据重心坐标获取值[todo]

--- raster-ts-v2/src/engine/data/shader-variable.ts
+++ raster-ts-v2/src/engine/data/shader-variable.ts
@@ -4,21 +4,62 @@ import { Vec4 } from "../../base/math/vec4";
  * 顶点着色器传给片段着色器的变量
  */
 export class ShaderVariable {
+    private alpha: number;
+    private beta: number;
+    private gamma: number;
+
+    public setBarycentric(alpha: number, beta: number, gamma: number): void {
+        this.alpha = alpha;
+        this.beta = beta;
+        this.gamma = gamma;
+    }
+
     /**
      * 坐标
      */
-    public v_position: Vec4;
+    private _position: Vec4;
+    public set position(val: Vec4) {
+        this._position = val;
+    }
+
+    public get position(): Vec4 {
+        return this._position;
+    }
+
     /**
      * 法线
      */
-    public v_normal: Vec4;
+    private _normal: Vec4;
+    public set normal(val: Vec4) {
+        this._normal = val;
+    }
+
+    public get normal(): Vec4 {
+        return this._normal;
+    }
+
     /**
      * uv信息
      */
-    public v_uv: Vec4;
+    private _uv: Vec4;
+    public set uv(val: Vec4) {
+        this._uv = val;
+    }
+
+    public get uv(): Vec4 {
+        return this._uv;
+    }
+
     /**
      * 颜色
      */
-    public v_color: Vec4;
+    private _color: Vec4;
+    public set color(val: Vec4) {
+        this._color = val;
+    }
+
+    public get color(): Vec4 {
+        return this._color;
+    }
 
 }
