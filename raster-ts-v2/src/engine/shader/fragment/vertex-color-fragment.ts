ba0112efa7ab69baee4e7dca56b91ef12261be57
王安康 <jecced@gmail.com>
2025-02-03 13:34:32 +0000

    feat: 新增一个输出顶点色的片段着色器

---
+++ raster-ts-v2/src/engine/shader/fragment/vertex-color-fragment.ts
@@ -0,0 +1,20 @@
+import { FragmentShader } from "./fragment-shader";
+import { GlData } from "../../data/gl-data";
+import { Vec4 } from "../../../base/math/vec4";
+import { ShaderVariable } from "../../data/shader-variable";
+
+/**
+ * 顶点色着色器
+ */
+export class VertexColorFragment implements FragmentShader {
+    main(glData: GlData, input: ShaderVariable): Vec4 {
+        return new Vec4(
+            input.color.x,
+            input.color.y,
+            input.color.z,
+            1,
+        );
+    }
+
+
+}
