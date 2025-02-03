93ee17f5cca2afef93c6b9b301be034d787d354c
王安康 <jecced@gmail.com>
2025-02-03 14:15:14 +0000

    todo: 临时注释掉没有用到的模型解析代码

--- raster-ts-v2/src/res/model-manage.ts
+++ raster-ts-v2/src/res/model-manage.ts
@@ -4,41 +4,40 @@ import { Node } from "../scene/node";
 export class ModelManage {
 
     // 非洲人
-    public static async getNode0(): Promise<Node> {
-
-        const model = await Res.loadHeadObj();
-
-        model.setMatUV("default", await Res.loadHeadImg());
-
-        return new Node(model);
-    }
-
-    // 大菠萝
-    public static async getNode1(): Promise<Node> {
-
-        const model = await Res.loadDiabloObj();
-
-        model.setMatUV("default", await Res.loadDiabloImg());
-
-        return new Node(model);
-    }
-
-
-    // 妙蛙种子
-    public static async getNode2(): Promise<Node> {
-
-        const model = await Res.loadBulbasaurObj();
-
-        model.setMatUV("mat1", await Res.loadBulbasaurImg1());
-        model.setMatUV("mat2", await Res.loadBulbasaurImg2());
-
-        for (let i = 0; i < model.v.length; i++) {
-            // 缩放一下
-            model.v[i].scale(0.2);
-            // 往下移动一点
-            model.v[i].y -= 0.5;
-        }
-
-        return new Node(model);
-    }
+    // public static async getNode0(): Promise<Node> {
+    //
+    //     const model = await Res.loadHeadObj();
+    //
+    //     model.setMatUV("default", await Res.loadHeadImg());
+    //
+    //     return new Node(model);
+    // }
+    //
+    // // 大菠萝
+    // public static async getNode1(): Promise<Node> {
+    //
+    //     const model = await Res.loadDiabloObj();
+    //
+    //     model.setMatUV("default", await Res.loadDiabloImg());
+    //
+    //     return new Node(model);
+    // }
+    //
+    //
+    // // 妙蛙种子
+    // public static async getNode2(): Promise<Node> {
+    //
+    //     const model = await Res.loadBulbasaurObj();
+    //
+    //     model.setMatUV("mat1", await Res.loadBulbasaurImg1());
+    //     model.setMatUV("mat2", await Res.loadBulbasaurImg2());
+    //
+    //     for (let i = 0; i < model.v.length; i++) {
+    //         // 缩放一下
+    //         model.v[i].scale(0.2);
+    //         // 往下移动一点
+    //         model.v[i].y -= 0.5;
+    //     }
+    //
+    //     return new Node(model);
 }
