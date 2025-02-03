9e0520d8ef6c85a605e8cc8bac46da0029d1b5b0
王安康 <jecced@gmail.com>
2025-02-03 12:50:47 +0000

    feat: 编写简易光栅器

---
+++ raster-ts-v2/src/engine/rasterizer/rasterizer.ts
@@ -0,0 +1,17 @@
+import { Vec4 } from "../../base/math/vec4";
+import { ShaderVariable } from "../data/shader-variable";
+import { FragmentShader } from "../shader/fragment/fragment-shader";
+import { GlData } from "../data/gl-data";
+
+/**
+ * 光栅化
+ */
+export interface Rasterizer {
+    run(
+        p0: Vec4, p1: Vec4, p2: Vec4,
+        v0: ShaderVariable, v1: ShaderVariable, v2: ShaderVariable,
+        fs: FragmentShader, glData: GlData,
+    ): void;
+
+    getFrameBuffer(): Float32Array;
+}
