fb0f22f9d25817503c50c2d0e79dff670ea60476
王安康 <jecced@gmail.com>
2025-02-07 14:05:39 +0000

    feat: 测试光照着色器的index

--- raster-ts-v2/src/index.ts
+++ raster-ts-v2/src/index.ts
@@ -23,44 +23,58 @@ import { RasterizerDepth } from "./engine/rasterizer/rasterizer-depth";
 import { RasterizerTriangle } from "./engine/rasterizer/rasterizer-triangle";
 import { ObjParser } from "./util/obj-parser";
 import { ResourceObj, ResourcePng } from "./resources/resources";
+import { FragLightLambert } from "./engine/shader/fragment/frag-light-lambert";
+import { FragLightTest } from "./engine/shader/fragment/frag-light-test";
+import { Vec3 } from "./base/math/vec3";
 
 
 async function initScene(width: number, height: number): Promise<Scene> {
     const scene = new Scene();
     const camera = new Camera(width, height, -1, -100, 90);
     camera.usePerspective();
-    camera.setPosition(0, 1, 2);
+    camera.setPosition(0, 0, 2);
     scene.setCamera(camera);
 
     const cube = new Node();
-    cube.setVBO(Primitives.cube(), 3, 0, 3, 0, 0);
+    cube.setVBO(Primitives.cube2(), 3, 2, 0, 3, 0);
     cube.setPosition(0, 0, -1);
-    cube.vs = new VertRotationY();
-    cube.fs = new FragVertexColor();
+    cube.vs = new VertSimple();
+    cube.fs = new FragLightTest();
+    cube.texture0 = new Texture(await Loader.loadImg(ResourcePng.Grid));
     scene.addChild(cube);
 
-    const floor = new Node();
-    floor.setVBO(Primitives.plane(), 3, 2, 3, 0, 0);
-    floor.setPosition(0, -1, -1);
-    floor.setScaleFull(5);
-    floor.vs = new VertSimple();
-    floor.fs = new FragTexture();
-    floor.texture0 = new Texture(await Loader.loadImg(ResourcePng.Grid));
-    scene.addChild(floor);
+    // const light = new Node();
+    // light.setVBO(Primitives.cube(), 3, 0, 3, 0, 0);
+    // light.setPosition(1.2, 1.0, -2);
+    // light.setScaleFull(0.2);
+    // light.vs = new VertSimple();
+    // light.fs = new FragLightLambert();
+    // scene.addChild(light);
 
-    const african = new Node();
-    african.setVBO(ObjParser.coverToVAO(await Loader.loadText(ResourceObj.African)), 3, 2, 0, 3, 0);
-    african.setPosition(1.5, 0, -1);
-    african.vs = new VertRotationY();
-    african.fs = new FragTexture();
-    african.texture0 = new Texture(await Loader.loadImg(ResourcePng.AfricanDiffuse));
-    scene.addChild(african);
 
+    // const floor = new Node();
+    // floor.setVBO(Primitives.plane(), 3, 2, 3, 0, 0);
+    // floor.setPosition(0, -1, -1);
+    // floor.setScaleFull(5);
+    // floor.vs = new VertSimple();
+    // floor.fs = new FragTexture();
+    // floor.texture0 = new Texture(await Loader.loadImg(ResourcePng.Grid));
+    // scene.addChild(floor);
+    //
+    // const african = new Node();
+    // african.setVBO(ObjParser.coverToVAO(await Loader.loadText(ResourceObj.African)), 3, 2, 0, 3, 0);
+    // african.setPosition(1.5, 0, -1);
+    // african.vs = new VertRotationY();
+    // african.fs = new FragTexture();
+    // african.texture0 = new Texture(await Loader.loadImg(ResourcePng.AfricanDiffuse));
+    // scene.addChild(african);
+    //
     const diablo = new Node();
     diablo.setVBO(ObjParser.coverToVAO(await Loader.loadText(ResourceObj.Diablo)), 3, 2, 0, 3, 0);
-    diablo.setPosition(-1.5, 0, -1);
-    diablo.vs = new VertRotationY();
-    diablo.fs = new FragTexture();
+    diablo.setPosition(2.5, 0, -1.5);
+    diablo.setScaleFull(1.5);
+    diablo.vs = new VertSimple();
+    diablo.fs = new FragLightTest();
     diablo.texture0 = new Texture(await Loader.loadImg(ResourcePng.DiabloDiffuse));
     scene.addChild(diablo);
 
@@ -71,6 +85,11 @@ async function initScene(width: number, height: number): Promise<Scene> {
 
 async function run() {
 
+    /**
+     * 创建渲染管线
+     */
+    const pipeline = new RenderingPipeline();
+
     // dom 调试文本初始化
     DomText.init();
 
@@ -78,36 +97,35 @@ async function run() {
     const width = canvas.width;
     const height = canvas.height;
 
+    // 普通光栅器
     const rasterizerNormal = new RasterizerNormal(width, height);
     CanvasRasterizerMapping.bind(canvas, rasterizerNormal);
+    pipeline.rasterizer.push(rasterizerNormal);
 
+    // 深度光栅器
     const depth = new WebCanvas("depth");
     const rasterizerDepth = new RasterizerDepth(width, height);
     CanvasRasterizerMapping.bind(depth, rasterizerDepth);
+    pipeline.rasterizer.push(rasterizerDepth);
 
+    // 三角形光栅器
     const triangle = new WebCanvas("triangle");
     const rasterizerTriangle = new RasterizerTriangle(width, height);
     CanvasRasterizerMapping.bind(triangle, rasterizerTriangle);
+    pipeline.rasterizer.push(rasterizerTriangle);
 
 
     const scene = await initScene(width, height);
     const camera = scene.getCamera();
 
-    /**
-     * 创建渲染管线
-     */
-    const pipeline = new RenderingPipeline();
-
-    //将普通光栅器加入管线
-    pipeline.rasterizer.push(rasterizerNormal);
-    pipeline.rasterizer.push(rasterizerDepth);
-    pipeline.rasterizer.push(rasterizerTriangle);
 
     //glData
     const glData = new GlData();
     // 只设置一次
     glData.screenSize = new Vec4(width, height, 1 / width, 1 / height);
     glData.matScreen = camera.getScreenMat();
+    glData.mainLitDir = new Vec3(0, 1, 0).normalize();
+    glData.mainLitColor = new Vec4(1, 1, 1, 1);
 
     const renderOnce = function() {
         // 记录开始渲染的时间
@@ -136,7 +154,7 @@ function render(scene: Scene, glData: GlData, pipeline: RenderingPipeline): void
     glData.matProjection = camera.getProjectionMat();
     glData.matOrthographic = camera.getOrthographicMat();
     glData.time = RenderingScheduler.getTime();
-    glData.cameraPos = camera.getPosition();
+    glData.cameraPos = camera.getPosition().xyz;
     for (let i = 0, len = scene.size(); i < len; i++) {
         const node = scene.getChild(i);
         // 每个模型设置一次
