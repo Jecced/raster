3ae282a8cf6646ee82ea17dce5c1368b411518f9
王安康 <jecced@gmail.com>
2025-02-07 14:08:19 +0000

    refactor: 移除了早期的go和ts项目

--- raster-go/model3d/base.go
+++
@@ -1,5 +0,0 @@
-package model3d
-
-type NodeBase interface {
-	Position() (float64, float64, float64)
-}
--- raster-ts/src/scene/base.ts
+++
@@ -1,5 +0,0 @@
-import { Vec4 } from "../engine/base/math/vec4";
-
-export interface Base {
-    getPosition(): Vec4;
-}
