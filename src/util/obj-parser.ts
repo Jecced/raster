1aafae66a5d980b09d6ca66d7315625464c78d61
王安康 <jecced@gmail.com>
2025-02-08 12:12:51 +0000

    fix: 修复obj模型解析器错误的问题

--- src/util/obj-parser.ts
+++ src/util/obj-parser.ts
@@ -8,37 +8,56 @@ export class ObjParser {
         const normal: number[] = [];
         const uvs: number[] = [];
         const indices: number[] = [];
-        const uvLines: string[] = [];
+
+        const v: string[] = [];
+        const vt: string[] = [];
+        const vn: string[] = [];
+        const f: string[] = [];
+
 
         for (let i = 0, len = split.length; i < len; i++) {
             line = split[i];
             line.trim();
             if (line.startsWith("v ")) {
-                const vertSplit = line.substring(2).trim().split(" ");
-                vertex.push(parseFloat(vertSplit[0]));
-                vertex.push(parseFloat(vertSplit[1]));
-                vertex.push(parseFloat(vertSplit[2]));
+                v.push(line.substring(2).trim());
+                // const vertSplit = line.substring(2).trim().split(" ");
+                // vertex.push(parseFloat(vertSplit[0]));
+                // vertex.push(parseFloat(vertSplit[1]));
+                // vertex.push(parseFloat(vertSplit[2]));
             } else if (line.startsWith("vt ")) {
+                vt.push(line.substring(3).trim())
                 // const uvsSplit = line.substring(3).trim().split(" ");
                 // uvs.push(parseFloat(uvsSplit[0]));
                 // uvs.push(parseFloat(uvsSplit[1]));
-                uvLines.push(line.substring(3).trim());
             } else if (line.startsWith("vn ")) {
-                const normalSplit = line.substring(3).trim().split(" ");
-                normal.push(parseFloat(normalSplit[0]));
-                normal.push(parseFloat(normalSplit[1]));
-                normal.push(parseFloat(normalSplit[2]));
+                vn.push(line.substring(3).trim())
+                // const normalSplit = line.substring(3).trim().split(" ");
             } else if (line.startsWith("f ")) {
-                const faceSplit = line.substring(2).trim().split(" ");
-                indices.push(parseFloat(faceSplit[0].split("/")[0]) - 1);
-                indices.push(parseFloat(faceSplit[1].split("/")[0]) - 1);
-                indices.push(parseFloat(faceSplit[2].split("/")[0]) - 1);
-
-                this.putUvs(faceSplit[0], uvLines, uvs);
-                this.putUvs(faceSplit[1], uvLines, uvs);
-                this.putUvs(faceSplit[2], uvLines, uvs);
+                f.push(line.substring(2).trim());
+                // const faceSplit = line.substring(2).trim().split(" ");
             }
         }
+
+        const set: Set<string> = new Set<string>();
+
+        let index = 0;
+
+        for (let i = 0, len = f.length; i < len; i++) {
+            const triangle = f[i].split(" ");
+            if (this.applyVert(set, triangle[0], v, vn, vt, vertex, normal, uvs)) {
+                indices.push(index);
+                index++;
+            }
+            if (this.applyVert(set, triangle[1], v, vn, vt, vertex, normal, uvs)) {
+                indices.push(index);
+                index++;
+            }
+            if (this.applyVert(set, triangle[2], v, vn, vt, vertex, normal, uvs)) {
+                indices.push(index);
+                index++;
+            }
+        }
+
         return {
             position: vertex,
             color: [],
@@ -49,11 +68,26 @@ export class ObjParser {
         };
     }
 
-    private static putUvs(f: string, uvLines: string[], uvs: number[]): void {
-        const vertIndex = parseFloat(f.split("/")[0]) - 1;
-        const uvIndex = parseFloat(f.split("/")[1]) - 1;
-        const split = uvLines[uvIndex].split(" ");
-        uvs[vertIndex * 2] = parseFloat(split[0]);
-        uvs[vertIndex * 2 + 1] = parseFloat(split[1]);
+    private static applyVert(
+        set: Set<string>, triangle: string,
+        v: string[], vn: string[], vt: string[],
+        vertex: number[], normal: number[], uvs: number[],
+    ): boolean {
+        triangle = triangle.trim();
+        if (set.has(triangle)) {
+            return false;
+        }
+        const block = triangle.split("/");
+        this.applyInfo(v[parseFloat(block[0].trim()) - 1], vertex, 3);
+        this.applyInfo(vt[parseFloat(block[1].trim()) - 1], uvs, 2);
+        this.applyInfo(vn[parseFloat(block[2].trim()) - 1], normal, 3);
+        return true;
+    }
+
+    private static applyInfo(line: string, info: number[], size: number): void {
+        const block = line.split(" ");
+        for (let i = 0, len = size; i < len; i++) {
+            info.push(parseFloat(block[i]))
+        }
     }
 }
