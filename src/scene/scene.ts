cd0f8e369c58e73d0095f1437d35ecec7126f823
王安康 <jecced@gmail.com>
2025-02-08 09:16:27 +0000

    feat: 加入了点光源的定义

--- src/scene/scene.ts
+++ src/scene/scene.ts
@@ -1,16 +1,11 @@
 import { Camera } from "./camera";
 import { Node } from "./node";
+import { SphereLight } from "./sphere-light";
 
 export class Scene {
     private camera: Camera;
+    private sphereLight: SphereLight;
     private child: Array<Node> = [];
-
-    constructor() {
-    }
-
-    public setCamera(camera: Camera): void {
-        this.camera = camera;
-    }
 
     public addChild(node: Node): void {
         this.child.push(node);
@@ -24,7 +19,19 @@ export class Scene {
         return this.child.length;
     }
 
+    public setCamera(camera: Camera): void {
+        this.camera = camera;
+    }
+
     public getCamera(): Camera {
         return this.camera;
+    }
+
+    public setSphereLight(sphereLight: SphereLight):void{
+        this.sphereLight = sphereLight;
+    }
+
+    public getSphereLight():SphereLight{
+        return this.sphereLight;
     }
 }
