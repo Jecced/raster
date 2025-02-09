6e1ecc4ed7aaab7b56ce58e415b8b0d0cd011966
王安康 <jecced@gmail.com>
2025-02-09 11:55:41 +0000

    feat: 新增墙体纹理和墙体法线纹理

--- src/resources/resources.ts
+++ src/resources/resources.ts
@@ -5,12 +5,16 @@ import diablo from "../../resources/diablo3/diablo3_pose.obj";
 import diablo_diffuse from "../../resources/diablo3/diablo3_pose_diffuse.png";
 import grid from "../../resources/comm/grid.png";
 import earth_map from "../../resources/comm/earthmap.jpg";
+import brick_wall from "../../resources/brickwall/brickwall.jpg"
+import brick_wall_normal from "../../resources/brickwall/brickwall_normal.jpg"
 
 export enum ResourcePng {
     Grid = grid,
     AfricanDiffuse = african_diffuse,
     DiabloDiffuse = diablo_diffuse,
     EarthMap = earth_map,
+    BrickWall = brick_wall,
+    BrickWallNormal = brick_wall_normal,
 }
 
 export enum ResourceObj {
