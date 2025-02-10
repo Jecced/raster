f3bc646adf3cdbbc8d8e660d50cf7384a6e4bb19
王安康 <jecced@gmail.com>
2025-02-10 16:02:50 +0000

    feat: 精简index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import "../css/index.css";
 import { Scene } from "./scene/scene";
-import { Camera } from "./scene/camera";
-import { Node } from "./scene/node";
 import { RenderingPipeline } from "./engine/pipeline/rendering-pipeline";
 import { WebCanvas } from "./h5/web-canvas";
 import { GlData } from "./engine/data/gl-data";
@@ -10,108 +8,19 @@ import { Calc } from "./base/math/calc";
 import { RenderingScheduler } from "./scene/scheduler/rendering-scheduler";
 import { TimeEvent, TimeEventEnum } from "./event/time-event";
 import { DomText } from "./event/dom-text";
-import { FragTexture } from "./engine/shader/fragment/frag-texture";
-import { Loader } from "./util/loader";
-import { Texture } from "./base/texture";
-import { VertRotationY } from "./engine/shader/vertex/vert-rotation-y";
-import { VertSimple } from "./engine/shader/vertex/vert-simple";
-import { FragVertexColor } from "./engine/shader/fragment/frag-vertex-color";
-import { Primitives } from "./engine/geometry/primitives";
 import { RasterizerNormal } from "./engine/rasterizer/rasterizer-normal";
 import { CanvasRasterizerMapping } from "./h5/canvas-rasterizer-mapping";
-import { RasterizerDepth } from "./engine/rasterizer/rasterizer-depth";
-import { RasterizerTriangle } from "./engine/rasterizer/rasterizer-triangle";
-import { ObjParser } from "./util/obj-parser";
-import { ResourceObj, ResourcePng } from "./resources/resources";
-import { FragLightLambert } from "./engine/shader/fragment/frag-light-lambert";
 import { Vec3 } from "./base/math/vec3";
-import { SphereLight } from "./scene/sphere-light";
-import { FragLightSphere } from "./engine/shader/fragment/frag-light-sphere";
-import { IcoSphere } from "./engine/geometry/ico-sphere";
-import { LoopMoveNode } from "./scene/custom/loop-move-node";
 import { Scene01 } from "./priview/scene01/scene01";
 import { Scene02 } from "./priview/scene02/scene02";
 import { Scene03 } from "./priview/scene03/scene03";
 import { Scene04 } from "./priview/scene04/scene04";
-
-
-async function initScene(width: number, height: number): Promise<Scene> {
-    const scene = new Scene();
-
-    // 设置摄像机信息
-    const camera = new Camera(width, height, -1, -100, 90);
-    camera.usePerspective();
-    camera.setPosition(0, 3, 5);
-    camera.lookAt(new Vec3(0, 0, 0));
-    scene.setCamera(camera);
-
-
-    // 设置点光源信息
-    const sphereLight = new SphereLight();
-    sphereLight.setPosition(1, 3, 3);
-    sphereLight.setColor(new Vec4(1, 1, 1, 1));
-    scene.setSphereLight(sphereLight);
-
-
-    // const cube = new Node();
-    // cube.setVBO(Primitives.cube(), 3, 2, 3, 3, 0);
-    // cube.setPosition(0, 0, 0);
-    // cube.vs = new VertRotationY();
-    // cube.fs = new FragVertexColor();
-    // cube.texture0 = new Texture(await Loader.loadImg(ResourcePng.Grid));
-    // scene.addChild(cube);
-
-    const light = new Node();
-    light.setVBO(Primitives.sphere(2), 3, 0, 0, 3, 0);
-    // 将位置设置成点光源位置
-    light.setPosition(sphereLight.getPosition().x, sphereLight.getPosition().y, sphereLight.getPosition().z);
-    light.setScaleFull(0.2);
-    light.vs = new VertSimple();
-    light.fs = new FragLightLambert();
-    scene.addChild(light);
-
-
-    const floor = new Node();
-    floor.setVBO(Primitives.plane(), 3, 2, 3, 3, 0);
-    floor.setPosition(0, -1.5, 0);
-    floor.setScaleFull(10);
-    floor.vs = new VertSimple();
-    floor.fs = new FragLightSphere(12, 64);
-    floor.texture0 = new Texture(await Loader.loadImg(ResourcePng.Grid));
-    scene.addChild(floor);
-    //
-    // const african = new Node();
-    // african.setVBO(ObjParser.coverToVAO(await Loader.loadText(ResourceObj.African)), 3, 2, 0, 3, 0);
-    // african.setPosition(0, 1, 3);
-    // african.setScaleFull(1);
-    // african.vs = new VertRotationY();
-    // african.fs = new FragLightSphere(2.2, 128);
-    // african.texture0 = new Texture(await Loader.loadImg(ResourcePng.AfricanDiffuse));
-    // scene.addChild(african);
-
-    const sphere = new LoopMoveNode(5, 0, 0, 1);
-    sphere.setVBO(Primitives.sphere(3), 3, 2, 3, 3, 0);
-    sphere.setPosition(0, 1, 0);
-    sphere.setScaleFull(2);
-    sphere.vs = new VertSimple();
-    sphere.fs = new FragLightSphere(0.5, 64);
-    sphere.texture0 = new Texture(await Loader.loadImg(ResourcePng.EarthMap));
-
-    scene.addChild(sphere);
-
-
-    // const diablo = new Node();
-    // diablo.setVBO(ObjParser.coverToVAO(await Loader.loadText(ResourceObj.Diablo)), 3, 2, 0, 3, 0);
-    // diablo.setPosition(3, 1, -2);
-    // diablo.setScaleFull(3);
-    // diablo.vs = new VertRotationY();
-    // diablo.fs = new FragLightSphere(10, 4);
-    // diablo.texture0 = new Texture(await Loader.loadImg(ResourcePng.DiabloDiffuse));
-    // scene.addChild(diablo);
-
-
-    return scene;
-}
+import { Scene05 } from "./priview/scene05/sceen05";
+import { Scene06 } from "./priview/scene06/scene06";
+import { Scene07 } from "./priview/scene07/scene07";
+import { Scene08 } from "./priview/scene08/scene08";
+import { RasterizerTriangle } from "./engine/rasterizer/rasterizer-triangle";
+import { RasterizerDepth } from "./engine/rasterizer/rasterizer-depth";
 
 
 async function run() {
@@ -134,19 +43,19 @@ async function run() {
     pipeline.rasterizer.push(rasterizerNormal);
 
     // 深度光栅器
-    // const depth = new WebCanvas("depth");
-    // const rasterizerDepth = new RasterizerDepth(width, height);
-    // CanvasRasterizerMapping.bind(depth, rasterizerDepth);
-    // pipeline.rasterizer.push(rasterizerDepth);
-    //
-    // // 三角形光栅器
-    // const triangle = new WebCanvas("triangle");
-    // const rasterizerTriangle = new RasterizerTriangle(width, height);
-    // CanvasRasterizerMapping.bind(triangle, rasterizerTriangle);
-    // pipeline.rasterizer.push(rasterizerTriangle);
+    const depth = new WebCanvas("depth");
+    const rasterizerDepth = new RasterizerDepth(width, height);
+    CanvasRasterizerMapping.bind(depth, rasterizerDepth);
+    pipeline.rasterizer.push(rasterizerDepth);
+
+    // 三角形光栅器
+    const triangle = new WebCanvas("triangle");
+    const rasterizerTriangle = new RasterizerTriangle(width, height);
+    CanvasRasterizerMapping.bind(triangle, rasterizerTriangle);
+    pipeline.rasterizer.push(rasterizerTriangle);
 
 
-    const scene = await Scene04.creat(width, height);
+    const scene = await Scene08.creat(width, height);
     const camera = scene.getCamera();
 
 
