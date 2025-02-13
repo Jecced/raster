99bcf45deac453d55a05a76b38ea3cd4409a6b58
王安康 <jecced@gmail.com>
2025-02-13 09:13:49 +0000

    feat: 加入箱子镜面高光贴图

--- src/resources/resources.ts
+++ src/resources/resources.ts
@@ -9,16 +9,26 @@ import earth_map from "../../resources/comm/earthmap.jpg";
 import moon from "../../resources/comm/moon.jpg";
 import brick_wall from "../../resources/brickwall/brickwall.jpg"
 import brick_wall_normal from "../../resources/brickwall/brickwall_normal.jpg"
+import container2_diffuse from "../../resources/container2/container2_diffuse.png"
+import container2_specular from "../../resources/container2/container2_specular.png"
 
 export enum ResourcePng {
     Grid = grid,
+
     AfricanDiffuse = african_diffuse,
     AfricanNm = african_nm,
+
     DiabloDiffuse = diablo_diffuse,
+
     EarthMap = earth_map,
+
     BrickWall = brick_wall,
     BrickWallNormal = brick_wall_normal,
+
     Moon = moon,
+
+    Container2Diffuse = container2_diffuse,
+    Container2Specular = container2_specular,
 }
 
 export enum ResourceObj {
