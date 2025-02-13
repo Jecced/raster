7b10b0a8a3b44eaa63294cb6de8ad7a9f5cb330a
王安康 <jecced@gmail.com>
2025-02-13 13:07:25 +0000

    feat: glsl语法糖, textureCube

--- src/engine/shader/glsl-grammar/glsl-texture.ts
+++ src/engine/shader/glsl-grammar/glsl-texture.ts
@@ -1,6 +1,8 @@
 import { Texture } from "../../../base/texture";
 import { Vec4 } from "../../../base/math/vec4";
 import { Vec2 } from "../../../base/math/vec2";
+import { TextureCube } from "../../../base/texture-cube";
+import { Vec3 } from "../../../base/math/vec3";
 
 /**
  * glsl 语法模拟, 纹理系列
@@ -19,4 +21,16 @@ export function texture2D(sampler2d: Texture, uv: Vec2): Vec4 {
         return new Vec4(1.0, 1.0, 0.0, 1.0);
     }
     return sampler2d.getColorByUV(uv.x, uv.y);
+}
+
+/**
+ * textureCube函数
+ * @param sample3d
+ * @param dir
+ */
+export function textureCube(sample3d: TextureCube, dir: Vec3): Vec4 {
+    if (!sample3d) {
+        return new Vec4(1.0, 1.0, 0.0, 1.0);
+    }
+    return sample3d.getColorByVec(dir.x, dir.y, dir.z);
 }
